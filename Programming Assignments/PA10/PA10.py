1104d7a6207191637cd015237f20bedc57988536
roryhuck <rory_huck@redlands.edu>
2026-04-06 19:01:22 -0700

    Day 23 and final project

--- Programming Assignments/PA10/PA10.py
+++ Programming Assignments/PA10/PA10.py
@@ -27,3 +27,24 @@ statesDF = statesDF.to_crs(epsg=4326)
 povertyDF = povertyDF.to_crs(epsg=4326)
 joinDF = povertyDF.sjoin(statesDF)
 stateDF = joinDF[joinDF['NAME_right'] == 'Colorado']
+stateDF.plot(column='IPR_EST', markersize=10)
+state_shape = statesDF[statesDF['NAME'] == 'Colorado']
+
+ax = state_shape.boundary.plot()
+
+stateDF.plot(ax=ax, column='IPR_EST', markersize=10)
+#4
+top10_poverty = stateDF.sort_values(by='IPR_EST', ascending=True).head(10)
+print(top10_poverty[['NAME_left', 'IPR_EST']])
+co_avg = stateDF['IPR_EST'].mean()
+print(co_avg)
+state_avgs = {}
+
+for state in joinDF['NAME_right'].unique():
+    temp = joinDF[joinDF['NAME_right'] == state]
+    state_avgs[state] = temp['IPR_EST'].mean()
+
+print(state_avgs)
+print("Colorado avg:", state_avgs['Colorado'])
+print("Overall avg:", sum(state_avgs.values())/len(state_avgs))
+#Question 4: The top 10 highest poverty schools in Colorado are Irving Elementary (104) and Central High School (110) for the income to poverity ratio estimates. This is a measure of a housholds income and how it compares to the fedaeral poverty levels. These schools have incomes well below the poverty line based on their low IPR estimates. The average IPR estimate in Colorado is about 359 which is higherthan the overall U.S. average of 316.
